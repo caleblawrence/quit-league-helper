d92ef2c75cf914c494da7b97ef9b060765df7f53
Caleb Lawrence <lawrence.calebc@gmail.com>
2021-02-06 12:40:48 -0600

    setup cacheing in check-user-matchhistory

--- pages/api/check-user-matchhistory.ts
+++ pages/api/check-user-matchhistory.ts
@@ -3,5 +3,6 @@ import { checkIfUsersArePlaying } from "../../util/leagueMatchHistoryService";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await checkIfUsersArePlaying();
+  res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
   return res.json({ status: "success" });
 };
